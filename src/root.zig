ca0e9d86d9a16e323c60abc4cb545637cf9cb524
Mike Foster <i.am.mike.foster@gmail.com>
2025-12-11 13:14:30 +0000

    remove old add function

--- src/root.zig
+++ src/root.zig
@@ -4,20 +4,12 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
 fn turn(_: i8, rotation: []const u8) u7 {
     switch (rotation[0]) {
         'R' => return 19,
         'L' => return 0,
         else => unreachable,
     }
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
 }
 
 test "11R8" {

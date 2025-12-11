605ba4d8aca88cfb570085e68ac861ee7e17e088
Mike Foster <i.am.mike.foster@gmail.com>
2025-12-11 13:28:25 +0000

    use start position & clicks in calcn

--- src/root.zig
+++ src/root.zig
@@ -4,12 +4,16 @@
 const std = @import("std");
 const testing = std.testing;
 
-fn turn(_: i8, rotation: []const u8) u7 {
+fn turn(start: i8, rotation: []const u8) i8 {
+    var factor: i8 = undefined;
     switch (rotation[0]) {
-        'R' => return 19,
-        'L' => return 0,
+        'R' => factor = 1,
+        'L' => factor = -1,
         else => unreachable,
     }
+    var clicks: i8 = undefined;
+    clicks = std.fmt.parseInt(i8, rotation[1..], 10) catch unreachable;
+    return start + (factor * clicks);
 }
 
 test "11R8" {
@@ -18,4 +22,8 @@ test "11R8" {
 
 test "19L19" {
     try testing.expect(turn(19, "L19") == 0);
+}
+
+test "0L1" {
+    try testing.expect(turn(0, "L1") == 99);
 }

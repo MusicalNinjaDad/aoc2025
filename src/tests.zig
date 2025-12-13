a49cb790e4ebee3e26d6f491dfe8082bd873c97c
Mike Foster <i.am.mike.foster@gmail.com>
2025-12-13 07:58:26 +0000

    add why root.zig

--- src/tests.zig
+++ src/tests.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const testing = std.testing;
-const Tests = @import("root.zig").Tests;
+const Tests = @import("root.zig").Tests; // cannot use "root" here: https://github.com/ziglang/zig/issues/17109
 
 test "all" {
     const t: Tests = .{};

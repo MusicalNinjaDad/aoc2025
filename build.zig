fc39e92ff92e7e099ceec6420c8cb523e96b8b7f
Mike Foster <i.am.mike.foster@gmail.com>
2025-12-13 08:09:41 +0000

    run "generated" tests as dedicated build activity

--- build.zig
+++ build.zig
@@ -57,9 +57,23 @@ pub fn build(b: *std.Build) void {
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
+    const test_generator = b.createModule(.{
+        .root_source_file = b.path("src/generate_tests.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const generated_tests = b.addTest(.{
+        .root_module = test_generator,
+        .filters = test_filters,
+    });
+
+    const run_generated_tests = b.addRunArtifact(generated_tests);
+
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_generated_tests.step);
 }
--- src/generate_tests.zig
+++ src/generate_tests.zig
@@ -2,10 +2,10 @@ const std = @import("std");
 const testing = std.testing;
 const Tests = @import("root.zig").Tests; // cannot use "root" here: https://github.com/ziglang/zig/issues/17109
 
-test "all1" {
+test "all2" {
     const t: Tests = .{};
     inline for (t.cases) |tc| {
-        std.log.warn("{}", .{tc});
+        std.log.warn("generated {}", .{tc});
         try tc.run();
     }
 }
